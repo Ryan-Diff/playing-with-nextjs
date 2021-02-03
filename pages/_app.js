f3eb0356c3cff218f8c681df392fc9661830ef02
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2021-02-03 10:06:05 -0800

    Fix spelling errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../stlyes/global.css'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../stlyes/utils.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[Full-Stack Software Developer who thrives in collaborative and fast-paced environments to tackle complex challenges. A background in customer service and sales give me a unique perspective on both the client’s needs, wants, and desires, as well as an ability to problem solve and come up with unique solutions that solve the root problems as well as uplift others.]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
